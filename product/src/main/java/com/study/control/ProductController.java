f2a808dc2c2a6406102a01baa47dcaf285617d99
wangtan <wangtan@didiglobal.com>
2020-12-16 12:16:22 +0800

    product模块调用fund模块

---
+++ product/src/main/java/com/study/control/ProductController.java
@@ -0,0 +1,58 @@
+package com.study.control;
+
+import com.study.common.response.ResultMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author wangtan
+ * @Date 2020/12/16
+ */
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    /**
+     * 依赖注入restTemplate
+     */
+    @Autowired
+    private RestTemplate restTemplate = null;
+
+    /**
+     * 在这段代码中，url的服务器和端口被定义为了FUND，与资金微服务的配置项spring.application.name是一致的，
+     * 这样Eureka服务治理中心就知道你在请求资金微服务，并且默认采用轮询策略做负载均衡。
+     * url里面存在着参数的占位定义,跟着使用了Map<String, Object>对象封装请求的参数，其中key和url中参数的占位定义要保持一致。
+     * 最后使用了RestTemplate的postForObject方法对资金微服务进行请求，
+     * 这样就可以获取资金微服务返回来的信息进行打印了。
+     * @param userId
+     * @param productId
+     * @param amount
+     * @return
+     */
+    @GetMapping("/purchase/{userId}/{productId}/{amount}")
+    public ResultMessage purchaseProduct(@PathVariable("userId") Long userId,
+                                         @PathVariable("productId") Long productId,
+                                         @PathVariable("amount")Double amount) {
+        System.out.println("扣减产品余额。");
+        //这里的FUND代表资金微服务，RestTemplate会自动负载均衡
+        String url="http://FUND/fund/account/balance/{userId}/{amount}";
+        //封住请求参数
+        Map<String,Object> params =new HashMap<>();
+        params.put("userId",userId);
+        params.put("amount",amount);
+        //请求资金微服务
+        ResultMessage rm=restTemplate.postForObject(url,null,ResultMessage.class,params);
+        //打印资金微服务返回的消息
+        System.out.println(rm.getMessage());
+        System.out.println("交易记录信息");
+        return new ResultMessage(true,"交易成功");
+
+    }
+}
--- product/src/main/java/com/study/product/ProductApplication.java
+++ product/src/main/java/com/study/product/ProductApplication.java
@@ -3,6 +3,9 @@ package com.study.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author wtan
@@ -10,6 +13,16 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ProductApplication {
+    //这里我们使用Spring Cloud提供的组件Ribbon来调用它，调用fund里面的接口
+    // Ribbon使用的是处理过的RestTemplate模板。为了在产品微服务中使用Ribbon，我们在ProductApplication中加入RestTemplate的初始化，
+
+    //1.负载均衡：它会使用某种策略进行路由，路由到具体的微服务实例上，在默认情况下，使用的是轮询策略。
+    @LoadBalanced
+    //创建spring bean
+    @Bean
+    public RestTemplate initRestTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
